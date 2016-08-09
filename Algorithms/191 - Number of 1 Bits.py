713c511ebe46425478e116ee3a02e98eaa4e4951
noname <noname19930930@gmail.com>
2016-08-10 00:12:09 +0800

    AC

---
+++ Algorithms/191 - Number of 1 Bits.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while n:
+            count += n % 2
+            n /= 2
+        return count
