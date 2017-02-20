dfa46c0419bc9ec768ffc7a6862af2057753265c
Chia-Jui Yeh <noname19930930@gmail.com>
2017-02-21 01:30:44 +0800

    AC

---
+++ Algorithms/cpp/171 - Excel Sheet Column Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int titleToNumber(string s) {
+        // var
+        int result = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            result *= 26;
+            result += s[i] - 'A' + 1;
+        }
+        return result;
+    }
+};
