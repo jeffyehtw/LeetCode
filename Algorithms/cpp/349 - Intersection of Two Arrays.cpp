09241ccbfe8f9f064cab3f3be30bf8a624853704
CHIA-JUI YEH <noname19930930@gmail.com>
2017-07-29 23:24:32 +0800

    AC

---
+++ Algorithms/cpp/349 - Intersection of Two Arrays.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        // init
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        
+        remove_duplicate(nums1);
+        remove_duplicate(nums2);
+        
+        // var
+        vector<int> result(nums1.size() + nums2.size());
+        vector<int>::iterator it;
+        
+        it = set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), result.begin());
+        result.resize(it - result.begin());
+
+        return result;
+    }
+    
+    void remove_duplicate(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); ) {
+            if (nums[i - 1] == nums[i])
+                nums.erase(nums.begin() + i);
+            else
+                i++;
+        }
+    }
+};
