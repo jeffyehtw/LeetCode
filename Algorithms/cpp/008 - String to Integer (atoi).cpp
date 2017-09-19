3243c6ee9014364ce190a8bd91a0980aca9a1302
noname0930 <noname19930930@gmail.com>
2017-09-19 14:26:14 +0800

    Update sol

--- Algorithms/cpp/008 - String to Integer (atoi).cpp
+++ Algorithms/cpp/008 - String to Integer (atoi).cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
     int myAtoi(string str) {
-        // remove space
-        while (str[0] == ' ')
-            str.assign(str, 1, str.length() - 1);
-        
-        if (str == "")
-            return 0;
-        
         // var
-        long long result = 0;
-        int sign = str[0] == '-' ? -1 : 1;
+        int index = 0;
+        long sign, result = 0;
         
-        // remove sign
-        if (str[0] == '+' || str[0] == '-')
-            str.assign(str, 1, str.length() - 1);
+        // skip invalid
+        while (str[index] == ' ')
+            index++;
+        
+        sign = str[index] == '-' ? -1 : 1;
+        index += issign(str[index]);
             
-        if (!isdigit(str[0]))
-            return 0;
-
-        for (int i = 0; i < str.length(); i++) {
-            if (!isdigit(str[i]))
-                break;
+        for (int i = index; i < str.length() && isdigit(str[i]); i++) {
             result *= 10;
             result += str[i] - '0';
             
-            if (result * sign > INT_MAX)
-                return INT_MAX;
-            if (result * sign < INT_MIN)
+            if (sign * result < INT_MIN)
                 return INT_MIN;
+            else if (sign * result > INT_MAX)
+                return INT_MAX;
         }
-        
-        result *= sign;
-        
-        if (result > INT_MAX)
-            return INT_MAX;
-        if (result < INT_MIN)
-            return INT_MIN;
-        
-        return result;
+        return sign * result;
+    }
+    
+    bool issign(char c) {
+        return c == '-' || c == '+';
     }
 };
