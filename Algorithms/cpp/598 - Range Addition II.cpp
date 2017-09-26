837873e96b6fc454debd303e6835137b1e1c7b45
noname0930 <noname19930930@gmail.com>
2017-09-27 01:27:25 +0800

    shorter solution

--- Algorithms/cpp/598 - Range Addition II.cpp
+++ Algorithms/cpp/598 - Range Addition II.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-    int maxCount(int m, int n, vector<vector<int>>& ops) {
-        if (ops.size() == 0)
-            return m * n;
-        
-        // var
-        int x = ops[0][0] - 1;
-        int y = ops[0][1] - 1;
-        
-        for (int i = 1; i < ops.size(); i++) {
-            x = min(x, ops[i][0] - 1);
-            y = min(y, ops[i][1] - 1);
+    int maxCount(int m, int n, vector<vector<int>>& ops) {        
+        for (int i = 0; i < ops.size(); i++) {
+            m = min(m, ops[i][0]);
+            n = min(n, ops[i][1]);
         }
-        
-        return (x + 1) * (y + 1);
+        return m * n;
     }
 };
