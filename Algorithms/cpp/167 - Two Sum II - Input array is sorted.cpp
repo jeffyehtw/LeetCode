bbc7909d2b698468075475f96b8ff77c3143500a
noname0930 <noname19930930@gmail.com>
2017-09-28 21:36:39 +0800

    faster solution

--- Algorithms/cpp/167 - Two Sum II - Input array is sorted.cpp
+++ Algorithms/cpp/167 - Two Sum II - Input array is sorted.cpp
@@ -3,18 +3,12 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         // var
         map<int, int> m;
-        vector<int> result;
         
         for (int i = 0; i < numbers.size(); i++) {
-            if (m.find(numbers[i]) != m.end()) {
-                result.push_back(m[numbers[i]] + 1);
-                result.push_back(i + 1);
-                break;
-            }
-            else {
-                m[target - numbers[i]] = i;
-            }
+            if (m[target - numbers[i]] > 0)
+                return vector<int>({ m[target - numbers[i]], i + 1 });
+            m[numbers[i]] = i + 1;  
         }
-        return result;
+        return vector<int>();
     }
 };
