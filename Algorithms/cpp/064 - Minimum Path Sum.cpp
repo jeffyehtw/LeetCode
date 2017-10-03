28787779a768cbdeee8dcaf2440358b4d76244f2
noname0930 <noname19930930@gmail.com>
2017-10-04 03:08:24 +0800

    shorter solution

--- Algorithms/cpp/064 - Minimum Path Sum.cpp
+++ Algorithms/cpp/064 - Minimum Path Sum.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
+        if (grid.size() == 0)
+            return 0;
+        
         // var
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), 0));
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp = grid;
         
         // init
-        dp[0][0] = grid[0][0];
-        for (int i = 1; i < n; i++)
-            dp[i][0] = grid[i][0] + dp[i - 1][0];
         for (int i = 1; i < m; i++)
-            dp[0][i] = grid[0][i] + dp[0][i - 1];
+            dp[i][0] += dp[i - 1][0];
+        for (int j = 1; j < n; j++)
+            dp[0][j] += dp[0][j - 1];
         
-        // run
-        for (int i = 1; i < n; i++)
-        for (int j = 1; j < m; j++)
-            dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
-                
-        return dp[n - 1][m - 1];
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] += min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
