e4454687be582a0dcd63d28b8f9c4516c46bd1d8
cjyeh <cjyeh@cs.nctu.edu.tw>
2017-10-06 05:07:02 +0800

    better solution

--- Algorithms/cpp/500 - Keyboard Row.cpp
+++ Algorithms/cpp/500 - Keyboard Row.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     vector<string> findWords(vector<string>& words) {
         // var
+        map<char, int> m;
         vector<string> result;
-        
-        for (int i = 0; i < words.size(); i++) {
-            // var
-            int ok = true;
-            int row = find_row(tolower(words[i][0]));
-            
-            for (int j = 1; j < words[i].length() && ok; j++) {
-                if (find_row(tolower(words[i][j])) != row)
-                    ok = false;
-            }
-            
-            if (ok)
-                result.push_back(words[i]);
-        }
-        return result;
-    }
-    
-    int find_row(char c) {
-        // var
-        string rows[3] = {
+        string keyboards[3] = {
             "qwertyuiop",
             "asdfghjkl",
             "zxcvbnm"
         };
         
-        for (int i = 0; i < 3; i++) {
-            if (rows[i].find(c) != -1)
-                return i;
+        // init
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < keyboards[i].length(); j++)
+                m[keyboards[i][j]] = i;
+        
+        for (int i = 0; i < words.size(); i++) {
+            // var
+            bool valid = true;
+            
+            for (int j = 1; j < words[i].length() && valid; j++)
+                if (m[tolower(words[i][0])] != m[tolower(words[i][j])])
+                    valid = false;
+            
+            if (valid)
+                result.push_back(words[i]);
         }
-        return -1;
+        
+        return result;
     }
 };
