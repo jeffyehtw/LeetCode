4c78c420915b5146d6be8b1503fc33c8876465a5
CHIA-JUI YEH <noname19930930@gmail.com>
2017-10-13 05:22:32 +0800

    less memory used solution

--- Algorithms/cpp/167 - Two Sum II - Input array is sorted.cpp
+++ Algorithms/cpp/167 - Two Sum II - Input array is sorted.cpp
@@ -2,12 +2,22 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         // var
-        map<int, int> m;
-        
-        for (int i = 0; i < numbers.size(); i++) {
-            if (m[target - numbers[i]] > 0)
-                return vector<int>({ m[target - numbers[i]], i + 1 });
-            m[numbers[i]] = i + 1;  
+        int left = 0;
+        int right = numbers.size() - 1;
+        vector<int> result;
+
+        while (left < right) {
+            
+            cout << left << " " << right << endl;
+            // var
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target)
+                return vector<int>({ left + 1, right + 1 });
+            else if (sum > target)
+                right--;
+            else
+                left++;
         }
         return vector<int>();
     }
