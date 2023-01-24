54e359177a12fe3065cc08add49460fc9f4d1e4a
yehcj <yehcj.tw@gmail.com>
2023-01-25 02:21:23 +0800

    rename

--- cpp/0643.cpp
+++ cpp/0643.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        // var
-        double max_val = -10000 * 30001;
-        
-        // init
-        nums.insert(nums.begin(), 0);
-        for (int i = 1; i < nums.size(); i++)
-            nums[i] += nums[i - 1];
-        
-        for (int i = 1; i < nums.size() - k + 1; i++)
-            max_val = max(max_val, double(nums[i + k - 1] - nums[i - 1]));
-            
-        return max_val / k;
+        double ret = -DBL_MAX;
+        double sum = 0;
+
+        if (nums.size() < k) {
+            return ret;
+        }
+
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        ret = max(ret, sum);
+
+        for (int i = k; i < nums.size(); i++) {
+            sum = sum - nums[i - k] + nums[i];
+            ret = max(ret, sum);
+        }
+
+        return ret / k;
     }
 };
