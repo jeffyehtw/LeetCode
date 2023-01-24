d95e33d3e7a1ffc8b05c5e16eb7d6d2f1bed7482
yehcj <yehcj.tw@gmail.com>
2023-01-25 02:48:50 +0800

    AC

--- cpp/0204.cpp
+++ cpp/0204.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n <= 2)
+        int count = 0;
+        vector<bool> primes(n, true);
+
+        if (n <= 2) {
             return 0;
-            
-        // var
-        int count = 1;
-        vector<bool> prime(n, true);
-        int m = prime.size();
-        
-        // init
-        for (long i = 3; i < n; i += 2) {
-            if (prime[i]) {
-                for (long j = i * i; j < n; j += i)
-                    prime[j] = false;
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (!primes[i]) {
+                continue;
+            }
+            for (int j = i << 1; j < n; j += i) {
+                primes[j] = false;
+            }
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (primes[i]) {
                 count++;
             }
         }
+
         return count;
     }
 };
