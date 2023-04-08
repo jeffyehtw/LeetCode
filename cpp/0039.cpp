197b8812f567dbb658434cbf0b839e078c848c69
yehcj <yehcj.tw@gmail.com>
2023-04-08 21:06:24 +0800

    AC

--- cpp/0039.cpp
+++ cpp/0039.cpp
@@ -1,32 +1,25 @@
 class Solution {
+private:
+    vector<vector<int>> ret;
+    void backtrack(vector<int>& c, int idx, vector<int> arr, int t) {
+        if (t == 0) {
+            ret.push_back(arr);
+            return;
+        } else if ((t < 0) || (idx > c.size() - 1)) {
+            return;
+        }
+
+        backtrack(c, idx + 1, arr, t);
+        while (t >= c[idx]) {
+            t -= c[idx];
+            arr.push_back(c[idx]);
+            backtrack(c, idx + 1, arr, t);
+        }
+    }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        if (candidates.size() <= 0)
-            return result;
-            
-        // pre
-        sort(candidates.begin(), candidates.end());
-        
-        DFS(vector<int>(), candidates, target, 0);
-        
-        return result;
+        vector<int> tmp;
+        backtrack(candidates, 0, tmp, target);
+        return ret;
     }
-    
-    void DFS(vector<int> path, vector<int> candidates, int target, int start) {
-        if (target == 0) {
-            result.push_back(path);
-            return;
-        }
-        else if (target < 0)
-            return;
-        
-        for (int i = start; i < candidates.size(); i++) {
-            path.push_back(candidates[i]);
-            DFS(path, candidates, target - candidates[i], i);
-            path.pop_back();
-        }
-    }
-    
-    // var
-    vector<vector<int>> result;
 };
