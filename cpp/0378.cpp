1a5e96419704f07b86829081350643173fc6cd45
yehcj <yehcj.tw@gmail.com>
2023-04-22 02:03:47 +0800

    AC

--- cpp/0378.cpp
+++ cpp/0378.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        // var
-        vector<int> flat;
-        
-        for (int i = 0; i < matrix.size(); i++)
-            flat.insert(flat.end(), matrix[i].begin(), matrix[i].end());
-            
-        sort(flat.begin(), flat.end());
-        
-        return flat[k - 1];
+        int m = matrix.size();
+        vector<int> idx(m, 0);
+        int min;
+        int row;
+
+        for (int i = 0; i < k; i++) {
+            min = INT_MAX;
+            row = -1;
+
+            for (int j = 0; j < m; j++) {
+                if ((idx[j] < m) && (min > matrix[j][idx[j]])) {
+                    min = matrix[j][idx[j]];
+                    row = j;
+                }
+            }
+
+            idx[row]++;
+        }
+
+        return min;
     }
 };
