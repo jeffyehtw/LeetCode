b7337f927dab08aa721bb989feaa50de07e480ef
yehcj <yehcj.tw@gmail.com>
2023-04-23 23:41:34 +0800

    AC

--- cpp/0540.cpp
+++ cpp/0540.cpp
@@ -1,31 +1,12 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        // var
-        int left = 0;
-        int right = nums.size() - 1;
-        
-        while (left < right) {
-            // var
-            int mid = (left + right) / 2;
-            
-            if (mid & 1) {
-                if (nums[mid - 1] == nums[mid])
-                    left = mid + 1;
-                else if (nums[mid] == nums[mid + 1])
-                    right = mid - 1;
-                else
-                    return mid;
-            }
-            else {
-                if (nums[mid - 1] == nums[mid])
-                    right = mid - 2;
-                else if (nums[mid] == nums[mid + 1])
-                    left = mid + 2;
-                else
-                    return mid;
-            }
+        int ret = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            ret ^= nums[i];
         }
-        return nums[left];
+
+        return ret;
     }
 };
