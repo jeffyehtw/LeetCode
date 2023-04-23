fd1c91b4c4d7f0cd3f12a97d1c625f9dbbdcd3ab
yehcj <yehcj.tw@gmail.com>
2023-04-24 00:03:04 +0800

    AC

--- cpp/0561.cpp
+++ cpp/0561.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        // var
-        int sum = 0;
-        
-        // init
+        int ret = 0;
+
         sort(nums.begin(), nums.end());
-        
-        // count
-        for(int i = 0; i < nums.size() - 1; i += 2) {
-            sum += min(nums[i], nums[i + 1]);
+        for (int i = 0; i < nums.size(); i += 2) {
+            ret += nums[i];
         }
-        
-        return sum;
+
+        return ret;
     }
 };
