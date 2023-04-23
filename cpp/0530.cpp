d4762537df229397f2be3a423b852d50c365078c
yehcj <yehcj.tw@gmail.com>
2023-04-23 23:28:03 +0800

    AC

--- cpp/0530.cpp
+++ cpp/0530.cpp
@@ -4,53 +4,51 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
+private:
+    int ret = INT_MAX;
+    int _max(TreeNode* root) {
+        if (root == NULL) {
+            return -1;
+        }
+        if (root->right) {
+            return _max(root->right);
+        } else {
+            return root->val;
+        }
+    }
+    int _min(TreeNode* root) {
+        if (root == NULL) {
+            return -1;
+        }
+        if (root->left) {
+            return _min(root->left);
+        } else {
+            return root->val;
+        }
+    }
+    void traversal(TreeNode* root) {
+        if (root == NULL) {
+            return;
+        }
+
+        if (root->left) {
+            traversal(root->left);
+            ret = min(ret, root->val - _max(root->left));
+        }
+        if (root->right) {
+            traversal(root->right);
+            ret = min(ret, _min(root->right) - root->val);
+        }
+    }
 public:
     int getMinimumDifference(TreeNode* root) {
         traversal(root);
-        return result;
+        return ret;
     }
-    
-    void traversal(TreeNode* root) {
-        if (!root)
-            return;
-        
-        int left = find_max(root->left);
-        int right = find_min(root->right);
-        
-        if (left > -1) {
-            result = min(result, root->val - left);
-            traversal(root->left);
-        }
-        if (right > -1) {
-            result = min(result, right - root->val);
-            traversal(root->right);
-        }
-    }
-    
-    int find_min(TreeNode* root) {
-        if (!root)
-            return -1;
-        
-        while (root->left)
-            root = root->left;
-
-        return root->val;
-    }
-    
-    int find_max(TreeNode* root) {
-        if (!root)
-            return -1;
-            
-        while (root->right)
-            root = root->right;
-            
-        return root->val;
-    }
-    
-    // var
-    int result = INT_MAX;
 };
