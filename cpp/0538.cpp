4cc3efc2428acff975065d48e351b5da2d8e9eb0
yehcj <yehcj.tw@gmail.com>
2023-04-23 23:40:15 +0800

    AC

--- cpp/0538.cpp
+++ cpp/0538.cpp
@@ -4,28 +4,31 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
+private:
+    int sum = 0;
+    void traversal(TreeNode* root) {
+        if (root == NULL) {
+            return;
+        }
+
+        if (root->right) {
+            traversal(root->right);
+        }
+        sum += root->val;
+        root->val = sum;
+        if (root->left) {
+            traversal(root->left);
+        }
+    }
 public:
     TreeNode* convertBST(TreeNode* root) {
         traversal(root);
         return root;
-    }
-private:
-    int sum = 0;
-    void traversal(TreeNode* root) {
-        if (!root)
-            return;
-        
-        if (root->right)
-            traversal(root->right);
-        
-        sum += root->val;
-        root->val = sum;
-        
-        if (root->left)
-            traversal(root->left);
     }
 };
