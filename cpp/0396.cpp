fcdaaacda9d208185afddc32fccdc779b760ce89
yehcj <yehcj.tw@gmail.com>
2023-04-23 11:47:31 +0800

    AC

--- cpp/0396.cpp
+++ cpp/0396.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int maxRotateFunction(vector<int>& A) {
-        if (A.size() == 0)
-            return 0;
-            
-        // var
+    int maxRotateFunction(vector<int>& nums) {
+        int ret = 0;
         int sum = 0;
-        int current = 0;
-        int previous = 0;
-        int n = A.size();
-        
-        // init
-        for (int i = 0; i < A.size(); i++) {
-            sum += A[i];
-            previous += A[i] * i;
+        int presum = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            presum += nums[i];
+            sum += i * nums[i];
         }
-        
-        int result = previous;
-        for (int i = 1; i < A.size(); i++) {
-            current = sum - n * A[n - i] + previous;
-            previous = current;
-            result = max(result, current);
+        ret = sum;
+        for (int i = 1; i <= n - 1; i++) {
+            sum += presum;
+            sum -= n * nums[n - i];
+            ret = max(ret, sum);
         }
-        
-        return result;
+
+        return ret;
     }
 };
--- cpp/0492.cpp
+++ cpp/0492.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
     vector<int> constructRectangle(int area) {
-        // var
-        vector<int> result;
-        int min_diff = INT_MAX;
-        
-        for (int i = 1; i < sqrt(area) + 1; i++) {
+        vector<int> ret(2, 0);
+
+        for (int i = 1; i <= pow(area, 0.5); i++) {
             if (area % i == 0) {
-                // var
-                int L = area / i;
-                int W = i;
-                
-                if (L < W)
-                    swap(L, W);
-                
-                if (min_diff < L - W) {
-                    continue;
-                }
-                else {
-                    min_diff = L - W;
-                    result = vector<int>{L, W};
-                }
+                ret[0] = area / i;
+                ret[1] = i;
             }
         }
-        
-        return result;
+
+        return ret;
     }
 };
