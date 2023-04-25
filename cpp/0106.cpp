d77c18879b906e84cd8a019c681e0df2c3ccb0c0
yehcj <yehcj.tw@gmail.com>
2023-04-26 01:29:38 +0800

    AC

--- cpp/0106.cpp
+++ cpp/0106.cpp
@@ -4,33 +4,39 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        if (inorder.size() == 0 || postorder.size() == 0)
+        TreeNode* ret;
+        vector<int> left;
+        vector<int> right;
+        int idx = 0;
+        
+        if (inorder.size() == 0) {
             return NULL;
-        
-        // var
-        int mid = 0;
-        TreeNode* root = new TreeNode(postorder.back());        
-        
-        // init
+        } else if (inorder.size() == 1) {
+            ret = new TreeNode(postorder.back());
+            postorder.pop_back();
+            return ret;
+        }
+
+        ret = new TreeNode(postorder.back());
         postorder.pop_back();
-        while (inorder[mid] != root->val)
-            mid++;
-        
-        if (inorder.size() == 1)
-            return root;
-        
-        vector<int> left = mid > 0 ? vector<int>(inorder.begin(), inorder.begin() + mid) : vector<int>();
-        vector<int> right = mid + 1 < inorder.size() ? vector<int>(inorder.begin() + mid + 1, inorder.end()) : vector<int>();
-        
-        root->right = right.size() > 0 ? buildTree(right, postorder) : NULL;
-        root->left = left.size() > 0 ? buildTree(left, postorder) : NULL;
-        
-        return root;
+        for (; idx < inorder.size() && inorder[idx] != ret->val; idx++);
+        if (idx > 0) {
+            left = vector(inorder.begin(), inorder.begin() + idx);
+        }
+        if (idx + 1 < inorder.size()) {
+            right = vector(inorder.begin() + idx + 1, inorder.end());
+        }
+        ret->right = buildTree(right, postorder);
+        ret->left = buildTree(left, postorder);
+
+        return ret;
     }
 };
--- cpp/0377.cpp
+++ cpp/0377.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        // var
-        vector<int> dp(target + 1);
-        dp[0] = 1;
-        
-        for (int i = 0; i <= target; i++) {
+        vector<double> dp(target + 1, 0);
+
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i <= target; i++) {
             for (int j = 0; j < nums.size(); j++) {
-                if (i >= nums[j]) {
+                if (i < nums[j]) {
+                    break;
+                } else if (i == nums[j]) {
+                    dp[i] += 1;
+                } else {
                     dp[i] += dp[i - nums[j]];
                 }
             }
         }
-        return dp.back();
+
+        return dp[target];
     }
 };
