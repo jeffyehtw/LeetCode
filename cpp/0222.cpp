2a8abf7049454e77d8a3b64d6d467814b11312ac
yehcj <yehcj.tw@gmail.com>
2023-04-26 02:17:11 +0800

    AC

--- cpp/0222.cpp
+++ cpp/0222.cpp
@@ -4,33 +4,31 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
+private:
+    int help(TreeNode* root, int index) {
+        if (root == NULL) {
+            return 0;
+        } else if ((root->left == NULL) && (root->right == NULL)) {
+            return index;
+        }
+        
+        return max(
+            help(root->left, index << 1),
+            help(root->right, (index << 1) + 1)
+        );
+    }
 public:
     int countNodes(TreeNode* root) {
-        // var
-        int left = depth(true, root);
-        int right = depth(false, root);
-        
-        if (left == right)
-            return (1 << left) - 1;
-        else 
-            return 1 + countNodes(root->left) + countNodes(root->right);
-    }
-    
-    int depth(bool dir, TreeNode* root) {
-        if (!root)
+        if (root == NULL) {
             return 0;
-        
-        // var
-        int result = 0;
-        
-        while (root) {
-            result++;
-            root = dir ? root->left : root->right;
+        } else {
+            return help(root, 1);
         }
-        return result;
     }
 };
--- cpp/0451.cpp
+++ cpp/0451.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        // var
-        string result = "";
-        pair<int, int> p[256];
-        
-        // init
-        for (int i = 0; i < 256; i++) {
-            p[i].first = i;
-            p[i].second = 0;
+        string ret;
+        vector<pair<char, int>> freq;
+
+        for (int i = 0; i < 10; i++) {
+            freq.push_back(make_pair('0' + i, 0));
         }
-        for (int i = 0; i < s.length(); i++)
-            p[s[i]].second++;
-            
-        // bubble sort
-        for (int i = 0; i < 256; i++) {
-            for (int j = 1; j < 256 - i; j++) {
-                // var
-                int af = p[j - 1].first;
-                int bf = p[j].first;
-                int as = p[j - 1].second;
-                int bs = p[j].second;
-                
-                if (as > bs || (as == bs && af > bf))
-                    swap(p[j - 1], p[j]);
+        for (int i = 0; i < 26; i++) {
+            freq.push_back(make_pair('a' + i, 0));
+        }
+        for (int i = 0; i < 26; i++) {
+            freq.push_back(make_pair('A' + i, 0));
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (isdigit(s[i])) {
+                freq[s[i] - '0'].second++;
+            } else if ((s[i] >= 'a') && (s[i] <= 'z')) {
+                freq[s[i] - 'a' + 10].second++;
+            } else {
+                freq[s[i] - 'A' + 10 + 26].second++;
             }
-            // add result
-            result += string(p[255 - i].second, char(p[255 - i].first));
         }
-        return result;
+        sort(freq.begin(), freq.end(), [](pair<char, int> a, pair<char, int> b) {
+            return a.second > b.second;
+        });
+        for (int i = 0; i < freq.size(); i++) {
+            ret += string(freq[i].second, freq[i].first);
+        }
+
+        return ret;
     }
 };
