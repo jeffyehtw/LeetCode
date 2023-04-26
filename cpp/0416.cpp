51cb25fb8649c144ab6ab98608293e0ccd4f8f00
yehcj <yehcj.tw@gmail.com>
2023-04-27 00:00:47 +0800

    AC

--- cpp/0416.cpp
+++ cpp/0416.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        // var
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        int target = sum >> 1;
-        vector<bool> dp(target + 1, 0);
-        
-        if (sum & 1)
+        int sum = 0;
+        vector<vector<bool>> dp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+        }
+        if (sum % 2 == 1) {
             return false;
-        
-        // init
-        dp[0] = 1;
-        
-        for (auto num : nums) {
-            for (int i = target; i >= num; i--) {
-                dp[i] = dp[i] || dp[i - num];
-            }
         }
         
-        return dp[target];
+        sum /= 2;
+        dp = vector<vector<bool>>(
+            nums.size() + 1, 
+            vector<bool>(sum + 1, false)
+        );
+        for (int i = 0; i < nums.size() + 1; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i < sum + 1; i++) {
+            dp[0][i] = false;
+        }
+        for (int i = 1; i < nums.size() + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (j - nums[i - 1] < 0) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];   
+                }
+            }
+        }
+
+        return dp[nums.size()][sum];
     }
 };
