4b0935f334d2f5f5af585a59f9989e723ee1dd7b
yehcj <yehcj.tw@gmail.com>
2023-04-26 21:34:02 +0800

    AC

--- cpp/0300.cpp
+++ cpp/0300.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        if (nums.size() == 0)
-            return 0;
-            
-        // var
-        vector<int> v(0);
-        
-        for (int i = 0; i < nums.size(); i++) {
-            // var
-            auto it = lower_bound(v.begin(), v.end(), nums[i]);
-            if (it == v.end())
-                v.push_back(nums[i]);
-            else if (nums[i] < *it)
-                *it = nums[i];
+        int ret = 1;
+        vector<int> dp(nums.size(), 1);
+
+        for (int i = 1; i < dp.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] <= nums[j]) {
+                    continue;
+                }
+                dp[i] = max(dp[i], 1 + dp[j]);
+            }
+            ret = max(ret, dp[i]);
         }
-        return v.size();
+        
+        return ret;
     }
 };
