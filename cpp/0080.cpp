288c59e44d9f39061123c3f27a4dbc7ba4fbbcfd
yehcj <yehcj.tw@gmail.com>
2023-04-30 15:58:04 +0800

    AC

--- cpp/0080.cpp
+++ cpp/0080.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // var
-        map<int, int> m;
-        
-        // init
-        for (int i = 0; i < nums.size(); i++) {
-            if (m.find(nums[i]) == m.end())
-                m[nums[i]] = 1;
-            else
-                m[nums[i]]++;
-        }
-        
-        // erase duplicates
-        for (int i = 0; i < nums.size(); ) {
-            if (m[nums[i]] > 2) {
-                m[nums[i]]--;
-                nums.erase(nums.begin() + i);
-            }
-            else
+        int cnt = 1;
+
+        for (int i = 1; i < nums.size(); ) {
+            if (nums[i] == nums[i - 1]) {
+                cnt++;
+                if (cnt >= 3) {
+                    nums.erase(nums.begin() + i);
+                } else {
+                    i++;
+                }
+            } else {
+                cnt = 1;
                 i++;
+            }
         }
+
         return nums.size();
     }
 };
