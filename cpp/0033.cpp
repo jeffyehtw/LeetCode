e77f161e951e2b7c6d1eed3f98326fb27fe8d3e0
yehcj <yehcj.tw@gmail.com>
2023-05-01 03:27:26 +0800

    AC

--- cpp/0033.cpp
+++ cpp/0033.cpp
@@ -1,40 +1,40 @@
 class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        if (nums.empty())
-            return -1;
-        else if (target == nums[0])
-            return 0;
-        
-        // var
-        int mid = - 1;
-        
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i - 1] > nums[i]) {
-                mid = i;
-                break;
+private:
+    int binary(vector<int>& nums, int left, int right, int target) {
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        
-        if (mid == -1) {
-            return _search(0, nums.size() - 1, nums, target);
-        }
-                
-        return target > nums[0] ? _search(0, mid - 1, nums, target) : _search(mid, nums.size() - 1, nums, target);
-    }
-    
-    int _search(int left, int right, vector<int> nums, int target) {
-        while (left <= right) {
-            // var
-            int mid = (left + right) / 2;
-            
-            if (nums[mid] == target)
-                return mid;
-            else if (nums[mid] > target)
-                right = mid - 1;
-            else
-                left = mid + 1;
-        }
         return -1;
+    }
+public:
+    int search(vector<int>& nums, int target) {
+        int max = nums[0];
+        int idx = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (max < nums[i]) {
+                max = nums[i];
+                idx = i;
+            }
+        }
+
+        if ((target > max) 
+            || ((idx + 1 < nums.size()) && (target < nums[idx + 1]))) {
+            return -1;
+        }
+
+        if ((target >= nums[0]) && (target <= max)) {
+            return binary(nums, 0, idx, target);
+        } else {
+            return binary(nums, idx + 1, nums.size() - 1, target);
+        }
     }
 };
