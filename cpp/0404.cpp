0e0f972d9a0838d95b6affe1771f24c29145d8e1
yehcj <yehcj.tw@gmail.com>
2023-05-01 23:16:37 +0800

    AC

--- cpp/0404.cpp
+++ cpp/0404.cpp
@@ -4,26 +4,37 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
+private:
+    bool leaf(TreeNode* root) {
+        if (root == NULL) {
+            return false;
+        } else {
+            return (root->left == NULL) && (root->right == NULL);
+        }
+    }
 public:
     int sumOfLeftLeaves(TreeNode* root) {
-        if (!root)
-            return 0;
-            
-        return helper(root, false);
-    }
-    
-    int helper(TreeNode* root, bool is_left) {
-        if (!root->left && !root->right)
-            return is_left ? root->val : 0;
-    
-        // var
-        int left = !root->left ? 0 : helper(root->left, true);
-        int right = !root->right ? 0 : helper(root->right, false);
-    
-        return left + right;
+        int ret = 0;
+
+        if (root == NULL) {
+            return ret;
+        }
+
+        if (root->left && leaf(root->left)) {
+            ret += root->left->val;
+        } else if (root->left) {
+            ret += sumOfLeftLeaves(root->left);
+        }
+        if (root->right) {
+            ret += sumOfLeftLeaves(root->right);
+        }
+        
+        return ret;
     }
 };
