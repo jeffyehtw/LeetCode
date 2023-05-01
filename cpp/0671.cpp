c2c1a06843e5f283d78d0f8691079d9394dea61f
yehcj <yehcj.tw@gmail.com>
2023-05-01 23:28:33 +0800

    AC

--- cpp/0671.cpp
+++ cpp/0671.cpp
@@ -10,27 +10,25 @@
  * };
  */
 class Solution {
+private:
+    set<int> st;
+    void traversal(TreeNode* root) {
+        if (root == NULL) {
+            return;
+        }
+        st.insert(root->val);
+        traversal(root->left);
+        traversal(root->right);
+    }
 public:
-    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        TreeNode* ret = NULL;
-        
-        if ((root1 == NULL) && (root2 == NULL)) {
-            return ret;
-        }
-        
-        if (root1 && root2) {
-            ret = new TreeNode(root1->val + root2->val);
+    int findSecondMinimumValue(TreeNode* root) {
+        traversal(root);
+        if (st.size() < 2) {
+            return -1;
         } else {
-            ret = new TreeNode(root1 ? root1->val : root2->val);
+            auto it = st.begin();
+            it++;
+            return *it;
         }
-
-        ret->left = mergeTrees(
-            (root1) ? root1->left : NULL, 
-            (root2) ? root2->left : NULL);
-        ret->right = mergeTrees(
-            (root1) ? root1->right : NULL,
-            (root2) ? root2->right: NULL);
-
-        return ret;
     }
 };
