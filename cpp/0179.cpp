dec7a0591a4e997f22a40b885bacf1370d6cce22
yehcj <yehcj.tw@gmail.com>
2023-05-03 02:09:20 +0800

    AC

--- cpp/0179.cpp
+++ cpp/0179.cpp
@@ -1,33 +1,23 @@
-bool compare(string x, string y) {
-    return x + y > y + x;
-}
-
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-        // var
-        vector<string> v;
-        int n = nums.size();
-        
-        // init
-        for (int i = 0; i < n; i++)
-            v.push_back(to_string(nums[i]));
-        
-        sort(v.begin(), v.end(), compare);
-        
-        return format(v);
-    }
-    
-    string format(vector<string> v) {
-        // var
-        string result;
-        for (int i = 0; i < v.size(); i++)
-            result += v[i];
-        
-        // avoid "00"
-        while (result[0] == '0' && result.length() > 1)
-            result.erase(result.begin());
-            
-        return result;
+        string ret = "";
+        vector<string> s;
+        int idx = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            s.push_back(to_string(nums[i]));
+        }
+        sort(s.begin(), s.end(), [](string a, string b) {
+            return (a + b) > (b + a);
+        });
+        for (int i = 0; i < s.size(); i++) {
+            if ((ret == "") && (s[i] == "0")) {
+                continue;
+            }
+            ret += s[i];
+        }
+
+        return (ret == "") ? "0" : ret;
     }
 };
