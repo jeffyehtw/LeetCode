021046e539ac6aa936fe401d89ef182e4e1774a7
yehcj <yehcj.tw@gmail.com>
2023-05-03 23:00:35 +0800

    AC

--- cpp/0046.cpp
+++ cpp/0046.cpp
@@ -1,19 +1,30 @@
 class Solution {
 private:
     vector<vector<int>> ret;
-    void _permute(vector<int>& nums, int n) {
-        if (n == nums.size() - 1) {
-            ret.push_back(nums);
+    vector<int> cur;
+    vector<bool> used;
+    void backtrack(vector<int>& nums, int idx) {
+        if (idx == nums.size()) {
+            ret.push_back(cur);
+            return;
         }
-        for (int i = n; i < nums.size(); i++) {
-            swap(nums[n], nums[i]);
-            _permute(nums, n + 1);
-            swap(nums[n], nums[i]);
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i]) {
+                continue;
+            }
+            cur.push_back(nums[i]);
+            used[i] = true;
+            backtrack(nums, idx + 1);
+            cur.pop_back();
+            used[i] = false;
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        _permute(nums, 0);
+        used = vector<bool>(nums.size(), false);
+        sort(nums.begin(), nums.end());
+        backtrack(nums, 0);
         return ret;
     }
 };
