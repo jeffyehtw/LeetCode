62637bbef6d248148cb270c2acb150c33e72d852
yehcj <yehcj.tw@gmail.com>
2023-05-03 23:22:44 +0800

    AC

--- cpp/0216.cpp
+++ cpp/0216.cpp
@@ -1,32 +1,24 @@
 class Solution {
 private:
     vector<vector<int>> ret;
-    void backtrack(vector<int> nums, int idx, int k, int n) {
-        if (n == 0) {
-            if (nums.size() == k) {
-                ret.push_back(nums);
-            } 
+    vector<int> cur;
+    void backtrack(int idx, int k, int n) {
+        if ((cur.size() == k) && (n == 0)) {
+            ret.push_back(cur);
             return;
-        } else if ((idx > n) || (nums.size() > k) || (idx > 9)) {
+        } else if ((cur.size() > k) || (n < 0)) {
             return;
         }
 
-        backtrack(nums, idx + 1, k, n);
-        nums.push_back(idx);
-        backtrack(nums, idx + 1, k, n - idx);
+        for (int i = idx; i <= 9; i++) {
+            cur.push_back(i);
+            backtrack(i + 1, k, n - i);
+            cur.pop_back();
+        }
     }
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
-        int sum = 0;
-        vector<int> nums;
-
-        if ((n < (1 + k) * k / 2) 
-            || (n > (19 - k) * k / 2)) {
-            return ret;
-        }
-
-        backtrack(nums, 1, k, n);
-
+        backtrack(1, k, n);
         return ret;
     }
 };
