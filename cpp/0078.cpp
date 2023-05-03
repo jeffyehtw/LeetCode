183fa86ff21d59790e9dc6111756cce965bcb806
yehcj <yehcj.tw@gmail.com>
2023-05-03 23:16:14 +0800

    AC

--- cpp/0078.cpp
+++ cpp/0078.cpp
@@ -1,23 +1,20 @@
 class Solution {
+private:
+    vector<vector<int>> ret;
+    vector<int> cur;
+    void backtrack(vector<int>& nums, int idx) {
+        ret.push_back(cur);
+
+        for (int i = idx; i < nums.size(); i++) {
+            cur.push_back(nums[i]);
+            backtrack(nums, i + 1);
+            cur.pop_back();
+        }
+    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ret;
-        unsigned int n = 1 << nums.size();
-
-        for (int i = 0; i < n; i++) {
-            int k = i;
-            vector<int> set;
-
-            for (int j = 0; j < nums.size(); j++) {
-                if (k & 1) {
-                    set.push_back(nums[j]);
-                }
-                k >>= 1;
-            }
-
-            ret.push_back(set);
-        }
-
+        sort(nums.begin(), nums.end());
+        backtrack(nums, 0);
         return ret;
     }
 };
