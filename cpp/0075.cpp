b686ad633e552346204f3ff2f939e3771e675d8d
yehcj <yehcj.tw@gmail.com>
2023-05-07 19:33:46 +0800

    AC

--- cpp/0075.cpp
+++ cpp/0075.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int min;
-        int idx;
-
+        int idx = 0;
+        vector<int> v(3, 0);
+        
         for (int i = 0; i < nums.size(); i++) {
-            min = nums[i];
-            idx = i;
-
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (min > nums[j]) {
-                    min = nums[j];
-                    idx = j;
-                }
+            v[nums[i]]++;
+        }
+        for (int i = 0; i < v.size(); i++) {
+            for (int j = 0; j < v[i]; j++) {
+                nums[idx] = i;
+                idx++;
             }
-
-            swap(nums[i], nums[idx]);
         }
     }
 };
