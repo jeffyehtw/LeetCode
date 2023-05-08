7cb6bb39119de502b01f07aa6532f4999a0d2e93
yehcj <yehcj.tw@gmail.com>
2023-05-08 23:35:34 +0800

    AC

---
+++ cpp/0152.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int ret;
+        vector<int> dpmin(nums.size(), 0);
+        vector<int> dpmax(nums.size(), 0);
+
+        dpmin[0] = nums[0];
+        dpmax[0] = nums[0];
+        ret = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            dpmin[i] = min({
+                nums[i],
+                dpmin[i - 1] * nums[i],
+                dpmax[i - 1] * nums[i]
+            });
+            dpmax[i] = max({
+                nums[i],
+                dpmin[i - 1] * nums[i],
+                dpmax[i - 1] * nums[i]
+            });
+            ret = max(ret, dpmax[i]);
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            cout << dpmin[i] << " ";
+        }
+        cout << endl;
+        for (int i = 0; i < nums.size(); i++) {
+            cout << dpmax[i] << " ";
+        }
+        cout << endl;
+
+        return ret;
+    }
+};
