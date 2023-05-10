b531a8d3ce8a4e756e35d581a4ba7c644042ee90
yehcj <yehcj.tw@gmail.com>
2023-05-10 23:58:36 +0800

    AC

--- cpp/0448.cpp
+++ cpp/0448.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        // tag
+        vector<int> ret;
+        vector<bool> seen(nums.size() + 1, false);
+        
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[abs(nums[i]) - 1] > 0)
-                nums[abs(nums[i]) - 1] = nums[abs(nums[i]) - 1] * -1;
+            seen[nums[i]] = true;
         }
-        
-        // fix
-        for (int i = 0; i < nums.size(); i++)
-            if (nums[i] > 0)
-                nums[i] = i + 1;
-        
-        // remove
-        for (int i = 0; i < nums.size();) {
-            if (nums[i] < 0)
-                nums.erase(nums.begin() + i);
-            else
-                i++;
+        for (int i = 1; i <= nums.size(); i++) {
+            if (seen[i]) {
+                continue;
+            } else {
+                ret.push_back(i);
+            }
         }
-        return nums;
+
+        return ret;
     }
 };
