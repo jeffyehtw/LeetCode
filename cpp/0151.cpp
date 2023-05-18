31ac7f9d01fa608a3e5e171aa199be0267baf688
yehcj <yehcj.tw@gmail.com>
2023-05-19 01:00:34 +0800

    AC

--- cpp/0151.cpp
+++ cpp/0151.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    void reverseWords(string &s) {
-        if (s == "")
-            return;
-            
-        s = format(split(s));
-    }
-    
-    vector<string> split(string str) {
-        // var
-        string tmp;
-        stringstream ss(str);
-        vector<string> result;
-        
-        while (ss >> tmp) {
-            result.push_back(tmp);
-            
-            if (ss.peek() == ' ')
-                ss.ignore();
+    string reverseWords(string s) {
+        string ret = "";
+        int left = 0;
+        int right = 0;
+
+        for (; left < s.length(); left++) {
+            if (isalpha(s[left]) || isdigit(s[left])) {
+                break;
+            }
         }
-        return result;
-    }
-    
-    string format(vector<string> strs) {
-        if (strs.size() == 0)
-            return "";
-            
-        // var
-        string s = strs.back();
+        right = left + 1;
+
+        while (right < s.length()) {
+            if (isalpha(s[right]) || isdigit(s[right])) {
+                right++;
+            } else {
+                string sub(s.begin() + left, s.begin() + right);
+                ret = sub + " " + ret;
+                for (left = right + 1; left < s.length(); left++) {
+                    if (isalpha(s[left]) || isdigit(s[left])) {
+                        break;
+                    }
+                }
+                right = left + 1;
+            }
+        }
+        if (left < s.length()) {
+            ret = string(s.begin() + left, s.begin() + right) + " " + ret;
+        }
+        if ((ret.length() > 0) && (ret[ret.length() - 1] == ' ')) {
+            ret = string(ret, 0, ret.length() - 1);
+        }
         
-        for (int i = strs.size() - 2; i > -1; i--)
-            s += " " + strs[i];
-            
-        return s;
+        return ret;
     }
 };
