684ce436b517a4b5cad7977ddf5ff1a1f194938f
yehcj <yehcj.tw@gmail.com>
2023-05-20 00:41:16 +0800

    AC

--- cpp/0443.cpp
+++ cpp/0443.cpp
@@ -1,32 +1,51 @@
 class Solution {
+private:
+    int help(vector<char>& chars, int idx, int num) {
+        vector<char> digits;
+
+        if (num == 1) {
+            return 0;
+        }
+
+        while (num > 0) {
+            digits.push_back(('0' + num % 10));
+            num /= 10;
+        }
+        for (int i = 0; i < digits.size(); i++) {
+            chars[idx + i] = digits[digits.size() - 1 - i];
+        }
+
+        return digits.size();
+    }
 public:
     int compress(vector<char>& chars) {
-        // var
-        int index = 0;
-        
-        while (index < chars.size()) {
-            // var
-            int count = 1;
-            int next = index + 1;
-            
-            while (index + 1 < chars.size() && chars[index] == chars[index + 1]) {
-                chars.erase(chars.begin() + index + 1);
-                count++;
-            }
-            
-            if (count == 1) {
-                index = next;
-                continue;
-            }
-            
-            while (count) {
-                chars.insert(chars.begin() + index + 1, '0' + count % 10);
-                count /= 10;
-                next++;
-            }
-            
-            index = next;
+        int cnt = 0;
+        int ret = 0;
+        char c;
+
+        if (chars.size() <= 1) {
+            return chars.size();
         }
-        return chars.size();
+
+        c = chars[0];
+        cnt = 1;
+        for (int i = 1; i < chars.size(); i++) {
+            if (chars[i] == c) {
+                cnt++;
+            } else {
+                chars[ret] = c;
+                ret++;
+                ret += help(chars, ret, cnt);
+                c = chars[i];
+                cnt = 1;
+            }
+        }
+        if (cnt > 0) {
+            chars[ret] = c;
+            ret++;
+            ret += help(chars, ret, cnt);
+        }
+
+        return ret;
     }
 };
