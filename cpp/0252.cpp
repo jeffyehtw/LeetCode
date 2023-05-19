1b881c634f76b6f0ea6d850a80e358006a1d7f2c
yehcj <yehcj.tw@gmail.com>
2023-05-19 23:47:41 +0800

    AC

---
+++ cpp/0252.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        int cur = 0;
+        vector<pair<int, int>> vp;
+
+        for (int i = 0; i < intervals.size(); i++) {
+            vp.push_back(make_pair(intervals[i][0], 1));
+            vp.push_back(make_pair(intervals[i][1], -1));
+        }
+        sort(vp.begin(), vp.end(), [](pair<int, int> a, pair<int, int> b) {
+            return (a.first == b.first) ? (a.second < b.second) : (a.first < b.first);
+        });
+        for (int i = 0; i < vp.size(); i++) {
+            cur += vp[i].second;
+            if (cur > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
