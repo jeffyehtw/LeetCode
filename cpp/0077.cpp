1f47422c9377975f0317e924be7daabbd1f94a4a
yehcj <yehcj.tw@gmail.com>
2023-05-29 00:21:31 +0800

    AC

--- cpp/0077.cpp
+++ cpp/0077.cpp
@@ -2,27 +2,20 @@ class Solution {
 private:
     vector<vector<int>> ret;
     vector<int> cur;
-    void backtrack(vector<int>& nums, int idx, int k) {
+    void backtrack(int n, int k, int idx) {
         if (cur.size() == k) {
             ret.push_back(cur);
             return;
         }
-
-        for (int i = idx; i < nums.size(); i++) {
-            cur.push_back(nums[i]);
-            backtrack(nums, i + 1, k);
+        for (int i = idx; i <= n; i++) {
+            cur.push_back(i);
+            backtrack(n, k, i + 1);
             cur.pop_back();
         }
     }
 public:
     vector<vector<int>> combine(int n, int k) {
-        vector<int> nums;
-
-        for (int i = 1; i <= n; i++) {
-            nums.push_back(i);
-        }
-        backtrack(nums, 0, k);
-
+        backtrack(n, k, 1);
         return ret;
     }
 };
