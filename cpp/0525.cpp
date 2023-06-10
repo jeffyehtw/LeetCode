b82e39389b651e5873a0bd074018329ba1ee106e
yehcj <yehcj.tw@gmail.com>
2023-06-11 00:13:21 +0800

    AC

--- cpp/0525.cpp
+++ cpp/0525.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        int cnt;
+        int cnt = 0;
         int ret = 0;
+        vector<int> cnts(nums.size(), 0);
         unordered_map<int, int> umap;
 
-        cnt = (nums[0] == 1) ? 1 : -1;
-        umap[cnt] = 0;
-        for (int i = 1; i < nums.size(); i++) {
+        umap[0] = 0;
+        for (int i = 0; i < nums.size(); i++) {
             cnt += (nums[i] == 1) ? 1 : -1;
-
             if (cnt == 0) {
-                ret = max(ret, i + 1);
-            } else if (umap.find(cnt) != umap.end()) {
-                if (i - umap[cnt] > 1) {
-                    ret = max(ret, i - umap[cnt]);
-                }
-            } else {
+                ret = i + 1;
+            } else if (umap.find(cnt) == umap.end()) {
                 umap[cnt] = i;
+            } else {
+                ret = max(ret, i - umap[cnt]);
             }
         }
 
