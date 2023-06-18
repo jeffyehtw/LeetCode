f832bc2b0ebcb28891ab50bcce2ce89abf4c8616
yehcj <yehcj.tw@gmail.com>
2023-06-18 18:45:35 +0800

    AC

--- cpp/0056.cpp
+++ cpp/0056.cpp
@@ -1,27 +1,23 @@
 class Solution {
-
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.size() < 2) {
-            return intervals;
-        }
+        vector<vector<int>> ret;
 
-        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
-            return a[0] < b[0];
+        sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b) {
+            return (a[0] == b[0]) ? (a[1] < b[1]) : (a[0] < b[0]);
         });
-        for (int i = 0; i < intervals.size(); ) {
-            if (i == intervals.size() - 1) {
-                break;
-            } else if (i < intervals.size() - 1) {
-                if (intervals[i][1] >= intervals[i + 1][0]) {
-                    intervals[i][1] = max(intervals[i][1], intervals[i + 1][1]);
-                    intervals.erase(intervals.begin() + i + 1);
-                } else {
-                    i++;
-                }
+
+        ret.push_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); i++) {
+            if (ret.back()[1] >= intervals[i][1]) {
+                continue;
+            } else if (ret.back()[1] >= intervals[i][0]) {
+                ret.back()[1] = intervals[i][1];
+            } else {
+                ret.push_back(intervals[i]);
             }
         }
 
-        return intervals;
+        return ret;
     }
 };
