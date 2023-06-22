bf37fa7f69913e0071400e072eb26f06c2f4f65b
yehcj <yehcj.tw@gmail.com>
2023-06-22 21:36:40 +0800

    AC

---
+++ cpp/1609.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool valid = true;
+    vector<int> levels;
+    void traverse(TreeNode* root, int level) {
+        if ((root == NULL) || !valid) {
+            return;
+        } else if (level % 2 != root->val % 2) {
+            valid = false;
+            return;
+        }
+        if (level > levels.size()) {
+            levels.push_back(root->val);         
+        } else {
+            if (level % 2 == 0) {
+                if (root->val < levels[level - 1]) {
+                    levels[level - 1] = root->val;
+                } else {
+                    valid = false;
+                }
+            } else {
+                if (root->val > levels[level - 1]) {
+                    levels[level - 1] = root->val;
+                } else {
+                    valid = false;
+                }
+            }
+        }
+        traverse(root->left, level + 1);
+        traverse(root->right, level + 1);
+    }
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        traverse(root, 1);
+        for (int i = 0; i < levels.size(); i++) {
+            cout << levels[i] << endl;
+        }
+        return valid;
+    }
+};
