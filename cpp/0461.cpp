fe5a1775c3fac973f3553cdbbff245bc2cc16855
yehcj <yehcj.tw@gmail.com>
2023-07-10 23:32:30 +0800

    AC

--- cpp/0461.cpp
+++ cpp/0461.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        // var
-        int result = 0;
-        
-        while (x || y) {
-            result += (x & 1 xor y & 2);
-            x >>= 1;
-            y >>= 1;
+        int ret = 0;
+        int z = x ^ y;
+
+        while (z > 0) {
+            ret += (z & 1);
+            z >>= 1;
         }
-        return result;
+
+        return ret;
     }
 };
