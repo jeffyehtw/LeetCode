92a736f51421da1c01df264235ee9a6faba56b7e
yehcj <yehcj.tw@gmail.com>
2023-08-31 23:22:54 +0800

    AC

--- cpp/0137.cpp
+++ cpp/0137.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        // var
-        int result = 0;
-        vector<int> sum(32, 0);
-        
-        for (int i = 0; i < 32; i++) {
-            int mask = 1 << i;
-            for (int j = 0; j < nums.size(); j++)
-                sum[i] += nums[j] & mask ? 1 : 0;
+        unordered_map<int, int> umap;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (umap.find(nums[i]) == umap.end()) {
+                umap[nums[i]] = 1;
+            } else {
+                umap[nums[i]]++;
+                if (umap[nums[i]] == 3) {
+                    umap.erase(nums[i]);
+                }
+            }
         }
-        
-        for (int i = 32 - 1; i > -1; i--)
-            result = (result << 1) + (sum[i] % 3);
-        
-        return result;
+
+        return umap.begin()->first;
     }
 };
