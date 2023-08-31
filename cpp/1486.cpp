e1fd0d3b774ec169266b5cb63d9d957ca85d4cab
yehcj <yehcj.tw@gmail.com>
2023-09-01 00:19:25 +0800

    AC

---
+++ cpp/1486.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int ret = 0;
+
+        for (int i = 0; i < n; i++) {
+            ret ^= start + i * 2;
+        }
+
+        return ret;
+    }
+};
